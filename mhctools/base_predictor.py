391657e492dbc865de5d72f512a03ba9540015e5
Shawn24 <78519750+zsxshawn@users.noreply.github.com>
2024-07-09 13:45:33 -0400

    Update base_predictor.py

--- mhctools/base_predictor.py
+++ mhctools/base_predictor.py
@@ -234,32 +234,36 @@ class BasePredictor(object):
                 peptide_lengths=peptide_lengths).to_dataframe()
 
     @staticmethod
-    def _check_hla_alleles(
-            alleles,
-            valid_alleles=None):
-        """
-        Given a list of HLA alleles and an optional list of valid
-        HLA alleles, return a set of alleles that we will pass into
-        the MHC binding predictor.
-        """
-        require_iterable_of(alleles, str, "HLA alleles")
+    def _check_hla_alleles(self, alleles, valid_alleles=None):
+    """
+    Given a list of HLA alleles and an optional list of valid
+    HLA alleles, return a set of alleles that we will pass into
+    the MHC binding predictor.
+    """
+    require_iterable_of(alleles, str, "HLA alleles")
 
-        # Don't run the MHC predictor twice for homozygous alleles,
-        # only run it for unique alleles
-        alleles = {
-            normalize_allele_name(allele.strip().upper())
-            for allele in alleles
-        }
-        if valid_alleles:
-            # For some reason netMHCpan drops the '*' in names, so
-            # 'HLA-A*03:01' becomes 'HLA-A03:01'
-            missing_alleles = [
-                allele
-                for allele in alleles
-                if allele not in valid_alleles
-            ]
-            if len(missing_alleles) > 0:
-                raise UnsupportedAllele(
-                    "Unsupported HLA alleles: %s" % missing_alleles)
+    # Don't run the MHC predictor twice for homozygous alleles,
+    # only run it for unique alleles
+    normalized_alleles = set()
+    for allele in alleles:
+        allele = allele.strip().upper()
+        if "CUSTOM" in allele:
+            normalized_alleles.add(allele)
+        else:
+            try:
+                normalized_alleles.add(normalize_allele_name(allele))
+            except AlleleParseError as e:
+                raise UnsupportedAllele(f"Unsupported allele {allele}: {e}")
 
-        return list(alleles)
+    if valid_alleles:
+        missing_alleles = [
+            allele
+            for allele in normalized_alleles
+            if allele not in valid_alleles
+        ]
+        if len(missing_alleles) > 0:
+            raise UnsupportedAllele(
+                "Unsupported HLA alleles: %s" % missing_alleles)
+
+    return list(normalized_alleles)
+
